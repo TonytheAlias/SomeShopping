f8edf720e10ef52fda80d5c515ab4aa4fdfc1b4c
Tyreek Brasfield <tyreekbrasfield@gmail.com>
2025-05-15 13:07:25 -0500

    Updated header css

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
+import headerImg from "../Images/Background-Header.jpg";
 import "../css/LandingPage.css";
 
 function LandingPage() {
   return (
-    <div className="containter">
-      <header>
+    <div className="container">
+      <div className="header-wrapper">
         <Header />
+        <img src={headerImg} alt="Header Background" className="header-bg" />
         <div className="find-fit">
           <h3>Find Your Fit</h3>
           <p>
@@ -14,7 +16,18 @@ function LandingPage() {
           </p>
           <button>Shop Now</button>
         </div>
-      </header>
+      </div>
+      <section>
+        <div className="clothes womens">
+          <h2>Womens</h2>
+        </div>
+        <div className="clothes mens">
+          <h2>Mens</h2>
+        </div>
+        <div className="clothes kids">
+          <h2>Kids</h2>
+        </div>
+      </section>
     </div>
   );
 }
